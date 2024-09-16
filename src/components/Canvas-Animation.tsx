6f174d7793e83f54fcdec9db5355931966fa7d58
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2024-09-16 15:08:37 +0200

    ...test performance

--- src/components/Canvas-Animation.tsx
+++ src/components/Canvas-Animation.tsx
@@ -1,6 +1,6 @@
 import { fromLandingPageState } from "~/landing-page-state";
 import { onMount, ParentComponent, createEffect } from "solid-js";
-import { Bound, CanvasSpace, Pt } from "pts";
+import { Bound, CanvasSpace, Line, Num, Pt } from "pts";
 import { gsap } from "gsap";
 import { createStore } from "solid-js/store";
 
@@ -30,6 +30,20 @@ export const CanvasAnimation: ParentComponent = (props) => {
 
   const green = "#3ef198";
 
+  let randX1 = 0;
+  let randX2 = 0;
+  let randX3 = 0;
+  let randX4 = 0;
+  let randY1 = 0;
+  let randY2 = 0;
+
+  createEffect(() => {
+    randX1 = Math.random() * landingPageState.totalWidth;
+    randX2 = Math.random() * landingPageState.totalWidth;
+    randY1 = Math.random() * landingPageState.screenHeight;
+    randY2 = Math.random() * landingPageState.screenHeight;
+  });
+
   onMount(() => {
     const space = new CanvasSpace("#canvas-animation").setup({
       bgcolor: "transparent",
@@ -37,85 +51,116 @@ export const CanvasAnimation: ParentComponent = (props) => {
       resize: true,
     });
 
-    const form = space.getForm();
-
     let lineOrigin = new Pt(0, 0);
     let lineTarget = new Pt(0, 0);
 
-    window.scrollTrigger.create({
-      trigger: "#anim-step-1",
-      start: "top top",
-      endTrigger: "#anim-step-2",
-      end: "top 30%",
-      onToggle: (self) => {
-        console.log("STEP 1", self.isActive);
-        setSection1({ ...section1, isActive: self.isActive });
-      },
-      onUpdate: (self) => {
-        setSection1({
-          ...section1,
-          progress: self.progress,
-          direction:
-            self.direction as unknown as AnimationSectionState["direction"],
-          velocity: self.getVelocity(),
-        });
-      },
-    });
-
-    window.scrollTrigger.create({
-      trigger: "#anim-step-2",
-      start: "top 30%",
-      endTrigger: "#anim-step-2",
-      end: "bottom+=100px 50%+=100px",
-      onToggle: (self) => {
-        console.log("STEP 2", self.isActive);
-        setSection2({ ...section2, isActive: self.isActive });
-      },
-      onUpdate: (self) => {
-        setSection2({
-          ...section2,
-          progress: self.progress,
-          direction:
-            self.direction as unknown as AnimationSectionState["direction"],
-          velocity: self.getVelocity(),
-        });
-      },
-    });
+    // window.scrollTrigger.create({
+    //   trigger: "#anim-step-1",
+    //   start: "top top",
+    //   endTrigger: "#anim-step-2",
+    //   end: "top 30%",
+    //   onToggle: (self) => {
+    //     console.log("STEP 1", self.isActive);
+    //     setSection1({ ...section1, isActive: self.isActive });
+    //   },
+    //   onUpdate: (self) => {
+    //     setSection1({
+    //       ...section1,
+    //       progress: self.progress,
+    //       direction:
+    //         self.direction as unknown as AnimationSectionState["direction"],
+    //       velocity: self.getVelocity(),
+    //     });
+    //   },
+    // });
+    //
+    // window.scrollTrigger.create({
+    //   trigger: "#anim-step-2",
+    //   start: "top 30%",
+    //   endTrigger: "#anim-step-2",
+    //   end: "bottom+=100px 50%+=100px",
+    //   onToggle: (self) => {
+    //     console.log("STEP 2", self.isActive);
+    //     setSection2({ ...section2, isActive: self.isActive });
+    //   },
+    //   onUpdate: (self) => {
+    //     setSection2({
+    //       ...section2,
+    //       progress: self.progress,
+    //       direction:
+    //         self.direction as unknown as AnimationSectionState["direction"],
+    //       velocity: self.getVelocity(),
+    //     });
+    //   },
+    // });
 
     createEffect(() => {
-      if (!section2.isActive && !section1.isActive) {
-        return;
-      }
+      // if (!section2.isActive && !section1.isActive) {
+      //   return;
+      // }
       const originPoint = new Pt(
-        Math.abs(section2.isActive ? landingPageState.totalWidth : 0),
-        0,
+        landingPageState.totalWidth / 2 +
+          (Math.cos(landingPageState.progress * 8) *
+            landingPageState.totalWidth) /
+            3,
+        landingPageState.screenHeight / 2 +
+          (Math.sin(landingPageState.progress * 8) *
+            landingPageState.screenHeight) /
+            3,
       );
       // Math.abs(section2.isActive ? landingPageState.screenHeight : 0)
 
-      const targetPoint = new Pt(
-        Math.abs(
-          section1.isActive ? section1.progress * 500 : section2.progress * 500,
-        ),
-        Math.abs(
-          section1.isActive ? section1.progress * 500 : section2.progress * 500,
-        ),
-      );
+      // const targetPoint = new Pt(
+      //   Math.abs(landingPageState.progress * landingPageState.totalWidth),
+      //   Math.abs(landingPageState.progress * landingPageState.screenHeight),
+      // );
       gsap.to(lineOrigin, {
         x: originPoint.x,
         y: originPoint.y,
-        duration: 1,
+        duration: 0.5,
       });
-      gsap.to(lineTarget, {
-        x: targetPoint.x,
-        y: targetPoint.y,
-        duration: 1,
-      });
+      // gsap.to(lineTarget, {
+      //   x: targetPoint.x,
+      //   y: targetPoint.y,
+      //   duration: 1,
+      // });
     }, [section1, section2]);
 
     space.add({
       start: (bound) => {},
       animate: (time, ftime) => {
-        form.stroke(green).line([lineOrigin, lineTarget]);
+        const numPoints = 12;
+        const lineA = [lineOrigin, new Pt(randX1, 0)];
+        const lineB = [
+          lineOrigin,
+          new Pt(randX2, landingPageState.screenHeight),
+        ];
+        const lineC = [lineOrigin, new Pt(0, randY1)];
+        const lineD = [lineOrigin, new Pt(landingPageState.totalWidth, randY2)];
+
+        const subPointsA = Line.subpoints(lineA, numPoints);
+        const subPointsB = Line.subpoints(lineB, numPoints);
+        const subPointsC = Line.subpoints(lineC, numPoints);
+        const subPointsD = Line.subpoints(lineD, numPoints);
+
+        for (let i = 0; i < numPoints; i++) {
+          space
+            .getForm()
+            .stroke(`transparent`)
+            .fill(`rgba(60,240,150,.02)`)
+            .polygon([
+              subPointsA[i],
+              subPointsC[i],
+              subPointsB[i],
+              subPointsD[i],
+            ]);
+        }
+
+        //
+        // circle
+        //   .stroke("transparent")
+        //   .fill(green)
+        //   .circle([lineOrigin, [3, 3]]);
       },
       action: (type, x, y) => {},
       resize: (bound: Bound, evt?: Event) => {
@@ -129,7 +174,7 @@ export const CanvasAnimation: ParentComponent = (props) => {
   return (
     <div>
       <canvas
-        class="fixed inset-0 pointer-events-none"
+        class="fixed inset-0 pointer-events-none "
         id="canvas-animation"
       ></canvas>
       <div class="relative">{props.children}</div>
