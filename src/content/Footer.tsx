4e1013ec277e806d8874f7bda5a3f9827a332e4c
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2024-09-24 19:20:15 +0200

    chore: fix footer styling

--- src/content/Footer.tsx
+++ src/content/Footer.tsx
@@ -6,11 +6,11 @@ import { FullWidth } from "~/components/layouts/Full-Width";
 export const Footer = () => {
   return (
     <footer id="footer">
-      <div class="min-h-32 px-6 md:px-0 sm:bg-3a-gray-darkest">
+      <div class="min-h-32 sm:px-6 md:px-0 sm:bg-3a-gray-darkest">
         <GridIndicator />
         <FullWidth>
           <div class="grid sm:grid-cols-2 xl:grid-cols-3 w-full">
-            <div class="py-4 px-4 md:p-8 flex h-full flex-col justify-center xl:col-span-2">
+            <div class="sm:p-4 md:p-8 flex h-full flex-col justify-center xl:col-span-2">
               <div>
                 <a href="/" title="to Homepage">
                   <MainLogo size={"small"} />
@@ -18,7 +18,7 @@ export const Footer = () => {
               </div>
             </div>
             <SmallText>
-              <div class="py-4 px-4 md:p-8 text-3a-paper">
+              <div class="sm:p-4 md:p-8 text-3a-paper">
                 <p class="font-normal  text-3a-white">Imprint</p>
                 <p>
                   Nikolaj Sokolowski
